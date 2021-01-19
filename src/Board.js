e978c3732e9651aeb774f6de41f90d46ec12ff60
Greg Foreman <gfore@mindspring.com>
2021-01-18 23:57:14 -0500

    add Game

--- src/Board.js
+++ src/Board.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import Square from './Square';
 import Knight from './Knight';
+import { moveKnight } from './Game'
+
+
+function handleSquareClick(toX, toY) {
+  moveKnight(toX, toY)
+}
 
 function renderSquare(i, [knightX, knightY]) {
   const x = i % 8
@@ -10,7 +16,11 @@ function renderSquare(i, [knightX, knightY]) {
   const piece = isKnightHere ? <Knight /> : null
 
   return (
-    <div key={i} style={{ width: '12.5vh', height: '12.5vh' }}>
+    <div
+      key={i}
+      style={{ width: '12.5vh', height: '12.5vh' }}
+      onClick={() => handleSquareClick(x, y)}
+    >
       <Square black={black}>{piece}</Square>
     </div>
   )
