b5a75fe9094df3ebbe768413434f793984f36f89
Greg Foreman <gfore@mindspring.com>
2021-02-02 00:03:18 -0500

    update Board

--- src/Board.js
+++ src/Board.js
@@ -1,47 +1,36 @@
 import React from 'react';
-import { DndProvider } from 'react-dnd'
-import { HTML5Backend } from 'react-dnd-html5-backend'
-// import { canMoveKnight, moveKnight } from './Game'
-import BoardSquare from './BoardSquare'
-import Knight from './Knight';
-// import Square from './Square';
-
-function renderSquare(i, knightPosition) {
-  const x = i % 8
-  const y = Math.floor(i / 8)
-  return (
-    <div key={i} style={{ width: '12.5%', height: '12.5%' }}>
-      <BoardSquare x={x} y={y}>
-        {renderPiece(x, y, knightPosition)}
-      </BoardSquare>
-    </div>
-  )
-}
-
-function renderPiece(x, y, [knightX, knightY]) {
-  if (x === knightX && y === knightY) {
-    return <Knight />
-  }
-}
-
-export default function Board({ knightPosition }) {
-  const squares = [];
-  for (let i = 0; i < 64; i++ ) {
-    squares.push(renderSquare(i, knightPosition));
-  }
-
-  return (
-    <DndProvider backend={HTML5Backend}>
-      <div
-        style={{
-          width: '100%',
-          height: '100%',
-          display: 'flex',
-          flexWrap: 'wrap',
-        }}
-      >
-        {squares}
-      </div>
-    </DndProvider>
-  );
-}
+import { BoardSquare } from './BoardSquare';
+import { Knight } from './Knight';
+/** Styling properties applied to the board element */
+const boardStyle = {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexWrap: 'wrap',
+};
+/** Styling properties applied to each square element */
+const squareStyle = { width: '12.5%', height: '12.5%' };
+/**
+ * The chessboard component
+ * @param props The react props
+ */
+export const Board = ({ knightPosition: [knightX, knightY], }) => {
+    function renderSquare(i) {
+        const x = i % 8;
+        const y = Math.floor(i / 8);
+        return (<div key={i} style={squareStyle}>
+				<BoardSquare x={x} y={y}>
+					{renderPiece(x, y)}
+				</BoardSquare>
+			</div>);
+    }
+    function renderPiece(x, y) {
+        const isKnightHere = x === knightX && y === knightY;
+        return isKnightHere ? <Knight /> : null;
+    }
+    const squares = [];
+    for (let i = 0; i < 64; i += 1) {
+        squares.push(renderSquare(i));
+    }
+    return <div style={boardStyle}>{squares}</div>;
+};
