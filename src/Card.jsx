1d0159defa33208968aeb2b8891f82f8d050bd0b
Greg Foreman <gfore@mindspring.com>
2021-03-03 13:51:13 -0500

    change backgroundColor and opacity on drag

--- src/Card.jsx
+++ src/Card.jsx
@@ -5,36 +5,42 @@ const style = {
     border: '1px dashed gray',
     padding: '0.5rem 1rem',
     marginBottom: '.5rem',
-    backgroundColor: 'white',
+    // backgroundColor: 'white',
     cursor: 'move',
 };
 export const Card = ({ id, text, moveCard, findCard }) => {
-    const originalIndex = findCard(id).index;
-    const [{ isDragging }, drag] = useDrag(() => ({
-        item: { type: ItemTypes.CARD, id, originalIndex },
-        collect: (monitor) => ({
-            isDragging: monitor.isDragging(),
-        }),
-        end: (dropResult, monitor) => {
-            const { id: droppedId, originalIndex } = monitor.getItem();
-            const didDrop = monitor.didDrop();
-            if (!didDrop) {
-                moveCard(droppedId, originalIndex);
-            }
-        },
-    }), [id, originalIndex]);
-    const [, drop] = useDrop(() => ({
-        accept: ItemTypes.CARD,
-        canDrop: () => false,
-        hover({ id: draggedId }) {
-            if (draggedId !== id) {
-                const { index: overIndex } = findCard(id);
-                moveCard(draggedId, overIndex);
-            }
-        },
-    }));
-    const opacity = isDragging ? 0 : 1;
-    return (<div ref={(node) => drag(drop(node))} style={{ ...style, opacity }}>
-			{text}
-		</div>);
+  const originalIndex = findCard(id).index;
+  const [{ isDragging }, drag] = useDrag(() => ({
+    item: { type: ItemTypes.CARD, id, originalIndex },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+    end: (dropResult, monitor) => {
+      const { id: droppedId, originalIndex } = monitor.getItem();
+      const didDrop = monitor.didDrop();
+      if (!didDrop) {
+        moveCard(droppedId, originalIndex);
+      }
+    },
+  }), [id, originalIndex]);
+
+  const [, drop] = useDrop(() => ({
+    accept: ItemTypes.CARD,
+    canDrop: () => false,
+    hover({ id: draggedId }) {
+        if (draggedId !== id) {
+            const { index: overIndex } = findCard(id);
+            moveCard(draggedId, overIndex);
+        }
+    },
+  }));
+
+  const opacity = isDragging ? 0.5 : 1;
+  const backgroundColor = isDragging ? 'lightgray' : 'white';
+  
+  return (
+    <div ref={(node) => drag(drop(node))} style={{ ...style, opacity, backgroundColor }}>
+      {text}
+    </div>
+  );
 };
