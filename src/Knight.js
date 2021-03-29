663048cda4ac2020f29b1256b217f7eb93f09380
Greg Foreman <gfore@mindspring.com>
2021-03-28 23:44:34 -0400

    improve knightStyle

--- src/Knight.js
+++ src/Knight.js
@@ -3,26 +3,28 @@ import { DragPreviewImage, useDrag } from 'react-dnd';
 import { ItemTypes } from './Constants';
 import { knightImage } from './knightImage';
 
-const knightStyle = {
+const knightStyle = (dragging) => (
+  {
     fontSize: 40,
     fontWeight: 'bold',
     cursor: 'move',
-};
+    opacity: dragging ? 0.5 : 1,
+  }
+);
 
 export const Knight = () => {
-    const [{ isDragging }, drag, preview] = useDrag({
-        item: { type: ItemTypes.KNIGHT },
-        collect: (monitor) => ({
-            isDragging: !!monitor.isDragging(),
-        }),
-    });
-    return (<>
-			<DragPreviewImage connect={preview} src={knightImage}/>
-			<div ref={drag} style={{
-        ...knightStyle,
-        opacity: isDragging ? 0.5 : 1,
-    }}>
-				♘
-			</div>
-		</>);
+  const [{ isDragging }, drag, preview] = useDrag({
+    item: { type: ItemTypes.KNIGHT },
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  });
+  return (
+    <>
+      <DragPreviewImage connect={preview} src={knightImage}/>
+      <div ref={drag} style={knightStyle(isDragging)}>
+        ♘
+      </div>
+    </>
+  );
 };
